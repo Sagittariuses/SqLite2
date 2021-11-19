838d0cd06a3ab411d4976841ae86513b17053f70
Sagittarius <ryaboyvlad1712@mail.ru>
2021-11-20 01:28:39 +0700

    Task2_SQLite

---
+++ app/src/main/java/com/example/sqlite2/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.sqlite2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView userList;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    SimpleCursorAdapter userAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userList = findViewById(R.id.list);
+        userList.setOnItemClickListener((parent, view, position, id) -> {
+            Intent intent = new Intent(getApplicationContext(), User.UserActivity.class);
+            intent.putExtra("id", id);
+            startActivity(intent);
+        });
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+        // создаем базу данных
+        databaseHelper.create_db();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // открываем подключение
+        db = databaseHelper.open();
+        //получаем данные из бд в виде курсора
+        userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE, null);
+        // определяем, какие столбцы из курсора будут выводиться в ListView
+        String[] headers = new String[]{DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_YEAR};
+        // создаем адаптер, передаем в него курсор
+        userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
+                userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        userList.setAdapter(userAdapter);
+    }
+
+    public void add(View view) {
+        Intent intent = new Intent(this, User.UserActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Закрываем подключение и курсор
+        db.close();
+        userCursor.close();
+    }
+}
+
+class DatabaseHelper extends SQLiteOpenHelper {
+    private static String DB_PATH; // полный путь к базе данных
+    private static final String DB_NAME = "cityinfo.db";
+    private static final int SCHEMA = 1; // версия базы данных
+    static final String TABLE = "users"; // название таблицы в бд
+    // названия столбцов
+    static final String COLUMN_ID = "_id";
+    static final String COLUMN_NAME = "name";
+    static final String COLUMN_YEAR = "year";
+    private final Context myContext;
+
+    DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, SCHEMA);
+        this.myContext = context;
+        DB_PATH = context.getFilesDir().getPath() + DB_NAME;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    void create_db() {
+
+        File file = new File(DB_PATH);
+        if (!file.exists()) {
+            //получаем локальную бд как поток
+            try (InputStream myInput = myContext.getAssets().open(DB_NAME);
+                 // Открываем пустую бд
+                 OutputStream myOutput = new FileOutputStream(DB_PATH)) {
+
+                // побайтово копируем данные
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = myInput.read(buffer)) > 0) {
+                    myOutput.write(buffer, 0, length);
+                }
+                myOutput.flush();
+            } catch (IOException ex) {
+                Log.d("DatabaseHelper", ex.getMessage());
+            }
+        }
+    }
+
+    public SQLiteDatabase open() throws SQLException {
+        return SQLiteDatabase.openDatabase(DB_PATH, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+}
